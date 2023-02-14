bda5719744383e117fc0cfc1b70623a2dba552c0
Fedor Lebed <flebedev777@gmail.com>
2023-02-14 13:23:18 +0300

    fixed website status function

--- scriptes/availability_checker.py
+++ scriptes/availability_checker.py
@@ -7,20 +7,26 @@ def availability_checker(url):
     req = Request(url)
     answer = []
     try:
+        flag = True
         for counter in range(5):
             response = urlopen(req)
-            time.sleep(7)
             if counter >= 3:
                 answer.append("The site is loading to slow")
             if response:
                 answer.append("Website is working fine")
-        answer.append("Couldn't load website")
+                flag = False
+                break
+            time.sleep(7)
+        if flag:
+            answer.append("Couldn't load website")
     except HTTPError as err:
         answer.append('The server couldn\'t fulfill the request.')
         answer.append(f'Error code: {err.code}')
     except URLError as err:
         answer.append('We failed to reach a server.')
         answer.append(f'Reason: {err.reason}')
-    else:
-        answer.append('Website is working fine')
     return answer
+
+
+if __name__ == '__main__':
+    print(availability_checker('https://aboba.io'))
