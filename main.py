816752b3e39fd34366984a3a1b76d5ef8157c87e
Cyber-Zhaba <20096ninja@gmail.com>
2023-02-15 19:39:05 +0300

    beta api

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ from flask import Flask, request
 from flask import render_template, redirect
 from flask_login import login_user, LoginManager, login_required, logout_user, current_user
 from flask_restful import Api
+from data.users_resource import UsersResource, UsersListResource
+from data.sites_resource import SitesResource, SitesListResource
+from data.feedback_resource import FeedbackResource, FeedbackListResource
 
 from data import db_session
 from data.sites import Sites
 from data.users import User
+from requests import get, post, delete
 
 from forms.registration_forms import RegisterForm, LoginForm
 
@@ -23,9 +27,18 @@ login_manager.init_app(app)
 
 
 def main():
+    api = Api(app)
+
+    api.add_resource(UsersListResource, '/api/users')
+    api.add_resource(UsersResource, '/api/users/<int:user_id>')
+
+    api.add_resource(SitesListResource, '/api/sites')
+    api.add_resource(SitesResource, '/api/sites/<int:site_id>')
+
+    api.add_resource(FeedbackListResource, '/api/feedback')
+    api.add_resource(FeedbackResource, '/api/feedback/<int:feedback_id>')
+
     db_session.global_init("data/data.db")
-    # api.add_resource(users_resource.UsersListResource, '/api/v2/users')
-    # api.add_resource(users_resource.UsersResource, '/api/v2/users/<int:user_id>')
 
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
@@ -58,7 +71,7 @@ def about_page():
 
 @app.route('/account')
 def account_page():
-    return redirect('/personal_account/&&%%')
+    return redirect('/personal_account/search=&&%%')
 
 
 @app.route('/logout')
@@ -87,14 +100,13 @@ def register():
             db_sess = db_session.create_session()
             if db_sess.query(User).filter(User.username == form.username.data).first():
                 return render_template('Register.html', form=form, message="Этот логин уже существует")
-            user = User(
-                username=form.username.data,
-                name=form.username.data,
-                email=form.email.data
-            )
-            user.set_password(form.password.data)
-            db_sess.add(user)
-            db_sess.commit()
+            post('http://localhost:5000/api/users', json={
+                'username': form.username.data,
+                'name': form.name.data,
+                'email': form.email.data,
+                'password': form.password.data
+            })
+            user = db_sess.query(User).filter(User.email == form.email.data).first()
             login_user(user, remember=form.remember_me.data)
             return redirect("/", 301)
         return render_template('Register.html', form=form)
@@ -113,7 +125,7 @@ def login():
             user = db_sess.query(User).filter(User.username == form.username.data).first()
             if user and user.check_password(form.password.data):
                 login_user(user, remember=form.remember_me.data)
-                return redirect("/personal_account/&&%%", 301)
+                return redirect("/", 301)
             return render_template('Login.html', message="Неправильный логин или пароль", form=form)
         return render_template('Login.html', form=form)
 
@@ -125,29 +137,33 @@ def personal_account(search):
         try:
             db_sess = db_session.create_session()
             if search == '&&%%':
-                favourite_sites_names = db_sess.query(Sites).filter(
+                favourite_sites_names_1 = db_sess.query(Sites).filter(
                     Sites.id.in_(current_user.favourite_sites.split(','))).all()
+                print(2)
+                favourite_sites_names = get("http://localhost:5000/api/sites", json={
+                    'type': 'favourite_sites',
+                    'favourite_sites': current_user.favourite_sites,
+                }).json()['sites']
+                print(3)
+                print(favourite_sites_names_1, favourite_sites_names)
+
                 not_favourite_sites_names = db_sess.query(Sites).filter(
                     ~ (Sites.id.in_(current_user.favourite_sites.split(',')))).all()
-                image_name = 'x'
             else:
                 favourite_sites_names = db_sess.query(Sites).filter(
                     Sites.name.contains(search), Sites.id.in_(current_user.favourite_sites.split(','))).all()
                 not_favourite_sites_names = db_sess.query(Sites).filter(
                     Sites.name.contains(search), ~(Sites.id.in_(current_user.favourite_sites.split(',')))).all()
-                image_name = f'{current_user.name}.png'
             return render_template('personal_account_table.html',
                                    favourite_sites=favourite_sites_names,
                                    not_favourite_sites=not_favourite_sites_names,
-                                   length=len(favourite_sites_names),
-                                   image_name=image_name)
+                                   length=len(favourite_sites_names))
         except Exception as error:
             if 'split' in error.__str__():
                 return render_template('personal_account_table.html',
                                        favourite_sites=[],
                                        not_favourite_sites=[],
-                                       length=0,
-                                       image_name=f'x')
+                                       length=0)
             else:
                 return redirect('/login')
 
@@ -156,21 +172,11 @@ def personal_account(search):
 def draw_graphic(website_id):
     time = range(1, 9)
     reports = [0, 0, 1, 3, 0, 5, 4, 5]
-    db_sess = db_session.create_session()
-    name = db_sess.query(Sites).filter(Sites.id.in_([website_id])).all()[0].name
+    plt.style.use(['dark_background'])
     plt.plot(time, reports)
-    fig, ax = plt.subplots(facecolor='#21024c')
-    ax.set_facecolor(color='#21024c')
-    ax.plot(reports, color='#0f497f')
-    ax.tick_params(axis='both', colors='white')
-    ax.spines['left'].set_color('white')
-    ax.spines['bottom'].set_color('white')
-    ax.spines['right'].set_color('#21024c')
-    ax.spines['top'].set_color('#21024c')
-    ax.grid(True)
-    ax.grid(linestyle='dashdot', linewidth=1, alpha=0.3)
-    fig.savefig(f'static/img/{current_user.name}.png', dpi=500)
-    return redirect(f'/personal_account/{name}')
+    plt.show()
+    plt.savefig('hohol.png')
+    return redirect('/personal_account')
 
 
 if __name__ == '__main__':
