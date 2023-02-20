a2d32951b11688ffe5fd1576763b95f276462f07
Alex <61513860+mav735@users.noreply.github.com>
2023-02-21 00:42:55 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,7 @@
 import os
 from aiogram import Bot
 
-bot = Bot(token=os.getenv('token'), parse_mode='HTML')
+bot = Bot("6250393018:AAHfyqaaFOqtD-SiTuRSfd9jZc2GlrtoYI8", parse_mode='HTML')
 
 """Potato Checker site url"""
 url = 'localhost:5000'
